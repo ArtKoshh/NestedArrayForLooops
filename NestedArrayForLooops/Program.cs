c027ec3066f6e2adc24030eb98b7b85ec94b777d
ArtKoshh <artur.koshmin@hotmail.com>
2024-08-24 20:19:05 +0200

    Add project files.

---
+++ NestedArrayForLooops/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace NestedArrayForLoops
+{
+    class Program
+    {
+        static int[,] matrix = 
+        { 
+            { 1, 2, 3 },
+            { 4, 5, 6 },
+            { 7, 8, 9 } 
+        };
+
+        static void Main(string[] args)
+        {
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write(matrix[i,j]+ " ");
+                }
+               
+            }
+            
+            
+            
+            
+            foreach (var item in matrix)
+            {
+                Console.Write(item + " ");
+            }
+            
+        }
+    }
+
+}
